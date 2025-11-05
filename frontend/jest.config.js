e16796d17cfa4db64388704187f1a30a725e892c
PengThreeGold <peng_three_gold@outlook.com>
2025-11-05 13:40:48 +0800

    [RESET]

--- frontend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/preset',
-  transform: {
-    '^.+\\.vue$': '@vue/vue3-jest',
-  },
-  testEnvironment: 'jsdom',
-  collectCoverageFrom: ['src/**/*.{js,vue}', '!src/main.js'],
-}
